2ec9f1e8e0acd014ab5262c33674c302476cc631
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2019-12-25 18:32:22 +0600

    SearchBar added to the ContentView.swift

--- SearchBar in SwiftUI/ContentView.swift
+++ SearchBar in SwiftUI/ContentView.swift
@@ -10,16 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var searchText : String
+    
     let arrayOfName = ["Manna","Munna","Shaon","Lalon","Limon","Rakibul","Shumon","Abid","Harun","Humaun","Liton","Milon"]
     
     
     var body: some View {
-        Text("Hello World")
+       
+        VStack(spacing: 5){
+            
+            SearchBar(text: $searchText)
+            
+            List(self.arrayOfName.filter{
+                self.searchText.isEmpty ? true : $0.localizedCaseInsensitiveContains(self.searchText)
+            } ,id: \.self){
+                Text($0)
+            }
+            
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(searchText: "")
     }
 }
